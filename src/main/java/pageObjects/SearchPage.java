020793fb98995d0cdbaf49b7e7658ee6b7598972
MaheshH91 <holkarmahesh11@gmail.com>
2025-10-07 15:34:32 +0530

    Second commit for search functionality

---
+++ src/main/java/pageObjects/SearchPage.java
@@ -0,0 +1,29 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SearchPage extends BasePage {
+	public SearchPage(WebDriver driver) {
+		super(driver);
+	}
+
+	@FindBy(linkText = "HP LP3065")
+	private WebElement validHPProduct;
+
+	@FindBy(xpath = "//div[@id='content']/h2/following-sibling::p")
+	private WebElement noProductMessage;
+
+	
+
+	public String retrieveNoProductMessageText() {
+		return noProductMessage.getText();
+	}
+
+	public boolean isHPValidProductDisplayed() {
+		return validHPProduct.isDisplayed();
+	}
+
+}
---
+++ src/main/java/testcases/TC004_Search_Test.java
@@ -0,0 +1,10 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class TC004_Search_Test {
+@Test
+public void verifySearchProduct() {
+	
+}
+}
